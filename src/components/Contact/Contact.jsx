4523352d46e3ec2b85ac4913edb46f16a59ea853
UnnatiBhal <109412356+UnnatiBhal@users.noreply.github.com>
2023-09-25 20:19:47 +0530

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import './Contact.css'
+import {MdCall} from 'react-icons/md';
+import {BsFillChatDotsFill} from 'react-icons/bs';
+import {HiChatBubbleBottomCenter} from 'react-icons/hi2'
+
+
+const Contact = () => {
+    return (
+
+        <section className="c-wrapper">
+            <div className=" paddings innerWidth flexCenter c-container">
+
+            {/* left side */}
+
+            <div className=" flexColStart c-left">
+            <span className="orangeText">Contact Us</span>
+            <span className="primaryText">Let's have a talk</span>
+            <span className="secondaryText">We always ready to help by providing the best services for you.
+            <br/>
+             We beleive a good place to live can make your life better.</span>
+
+             {/* to make contact modes */}
+
+             <div className="flexColStart contactModes">
+
+             {/* first row */}
+             <div className="flexStart row">
+             <div className="flexColCenter mode">
+             <div classname="flexStart">
+             <div className="flexCenter icon">
+             <MdCall size={25}/>
+             </div>
+             <div className=" flexColStart detail">
+             <span className="primaryText">Call</span>
+             <span className="secondaryText">021 345 231 21</span>
+
+             </div> 
+
+             {/* <div className="flexCenter button">Call Now</div> */}
+
+             </div>
+            <div className="flexCenter button">Call Now</div>
+
+             </div>
+
+             {/* 2nd mode */}
+
+             <div className="flexColCenter mode">
+             <div classname="flexStart">
+             <div className="flexCenter icon">
+             <BsFillChatDotsFill size={25}/>
+             </div>
+             <div className=" flexColStart detail">
+             <span className="primaryText">Chat</span>
+             <span className="secondaryText">021 345 231 21</span>
+
+             </div> 
+
+             
+
+             </div>
+            <div className="flexCenter button">Chat Now</div>
+
+             </div>
+
+             </div>
+
+             {/* 2nd row */}
+
+             <div className="flexStart row">
+             <div className="flexColCenter mode">
+             <div classname="flexStart">
+             <div className="flexCenter icon">
+             <BsFillChatDotsFill size={25}/>
+             </div>
+             <div className=" flexColStart detail">
+             <span className="primaryText">Video Call</span>
+             <span className="secondaryText">021 345 231 21</span>
+
+             </div> 
+
+             {/* <div className="flexCenter button">Call Now</div> */}
+
+             </div>
+            <div className="flexCenter button"> Vedio Call Now</div>
+
+             </div>
+
+             {/* 4th mode */}
+
+             <div className="flexColCenter mode">
+             <div classname="flexStart">
+             <div className="flexCenter icon">
+             <HiChatBubbleBottomCenter size={25}/>
+             </div>
+             <div className=" flexColStart detail">
+             <span className="primaryText">Message</span>
+             <span className="secondaryText">021 345 231 21</span>
+
+             </div> 
+
+             
+
+             </div>
+            <div className="flexCenter button">Message Now</div>
+
+             </div>
+
+             </div>
+             </div>
+             </div>
+
+             
+
+            
+
+            {/* right side */}
+
+            <div className="c-right">
+            <div className="img-container">
+                <img src="./contact.jpg" alt=""/>
+            </div>
+
+            </div>
+            
+
+            </div>
+        </section>
+
+    );
+};
+
+export default Contact;
---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './GetStarted.css'
+
+const GetStarted = () => {
+    return (
+
+        <section className="g-wrapper">
+            <div className="paddings innerWidth g-container">
+            <div className=" flexColCenter inner-container">
+            <span className="primaryText">Get Started with Homyz</span>
+            <span className="secondaryText">Subscribe and find super attractive price quotes from us.
+           
+            </span>
+
+            <button className="button">
+                <a href="mailto:unnati1bhal@gmail.com">Get Started</a>
+            </button>
+            
+
+            </div>
+                
+            </div>
+        </section>
+
+    );
+}
+
+export default GetStarted;
